fb39c390d2a0767d76af427a0a4c96b1e18e59b9
andhikadanis07 <andhikadanisw07@gmail.com>
2024-03-15 01:16:23 +0700

    Tugas Praktikum Pemlan 2

--- perpusatkaanOnline/booklist.java
+++ perpusatkaanOnline/booklist.java
@@ -43,7 +43,7 @@ public class booklist {
             jawaban = scn.next();
         } while (jawaban.equalsIgnoreCase("y"));
     }
-    
+
     static void tampilanBuku(String[][] jenisBuku, String kategoriBuku) {
         System.out.println("\n========[" + kategoriBuku + "]========");
         int bukuKe = 1;
@@ -52,8 +52,33 @@ public class booklist {
             System.out.println("1. Nama Buku: " + buku[0] 
             + "\n2. Penulis: " + buku[1]
             + "\n3. Jumlah Halaman: " + buku[2]
-            + "\n4. Tahun Terbit: " + buku[3]);
+            + "\n4. Tahun Terbit: " + buku[3]
+            + "\n5. Sinopsis : " + buku[4]
+            + "\n6. Jumlah kata Sinopsis : " + buku[4].split("\\s").length);
             bukuKe++;
         }
+    }
+
+    static int hitungJumlahKata(String sinopsis) {
+        return sinopsis.split("\\s+").length;
+    }
+
+    static double cekKesamaan(databook buku1, databook buku2) {
+        int totalKesamaan = 0;
+        int totalAtribut = 0;
+        String[][][] kategoriBuku1 = {buku1.teknologi, buku1.filsafat, buku1.sejarah, buku1.agama, buku1.psikologi, buku1.politik, buku1.fiksi};
+        String[][][] kategoriBuku2 = {buku2.teknologi, buku2.filsafat, buku2.sejarah, buku2.agama, buku2.psikologi, buku2.politik, buku2.fiksi};
+        
+        for (int k = 0; k < kategoriBuku1.length; k++) {
+            for (int i = 0; i < kategoriBuku1[k].length; i++) {
+                for (int j = 0; j < kategoriBuku1[k][i].length; j++) {
+                    if (kategoriBuku1[k][i][j].equals(kategoriBuku2[k][i][j])) {
+                        totalKesamaan++;
+                    }
+                    totalAtribut++;
+                }
+            }
+        }
+        return ((double) totalKesamaan / totalAtribut) * 100;
     }
 }
--- perpusatkaanOnline/databook.java
+++ perpusatkaanOnline/databook.java
@@ -2,60 +2,58 @@ package perpusatkaanOnline;
 
 public class databook {
     String[][] teknologi = {
-        {"The Phoenix Project: A Novel about IT, DevOps, and Helping Your Business Win", "Gene Kim, Kevin Behr, dan George Spafford", "343", "2013"},
-        {"The Innovators: How a Group of Hackers, Geniuses, and Geeks Created the Digital Revolution", "Walter Isaacson", "560", "2014"}, 
-        {"Algorithms to Live By: The Computer Science of Human Decisions", "Brian Christian dan Tom Griffiths","368", "2016"}, 
-        {"Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future", "Ashlee Vance", "400", "2015"}, 
-        {"Clean Code: A Handbook of Agile Software Craftsmanship", " Robert C. Martin", "464", "2008"}
+        {"The Phoenix Project: A Novel about IT, DevOps, and Helping Your Business Win", "Gene Kim, Kevin Behr, dan George Spafford", "343", "2013","Novel yang bercerita tentang transformasi IT dalam dunia bisnis modern."},
+        {"The Innovators: How a Group of Hackers, Geniuses, and Geeks Created the Digital Revolution", "Walter Isaacson", "560", "2014"," Novel yang mengulas kisah inovasi digital dari para tokoh pionir handal di dunia."}, 
+        {"Algorithms to Live By: The Computer Science of Human Decisions", "Brian Christian dan Tom Griffiths","368", "2016","Novel yang menjelaskan tentang pemadukan sains komputer dengan keputusan manusia sehari-hari."}, 
+        {"Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future", "Ashlee Vance", "400", "2015","Novel yang mengisahkan perjalanan dan ambisi pengusaha hebat, Elon Musk."}, 
+        {"Clean Code: A Handbook of Agile Software Craftsmanship", " Robert C. Martin", "464", "2008","Buku yang berisi tentang panduan penting untuk meningkatkan kualitas kode perangkat lunak."}
     };
 
     String[][] filsafat = {
-        {"Being and Time (Sein und Zeit)", "Martin Heidegger", "589", "1927"}, 
-        {"Critique of Pure Reason (Kritik der reinen Vernunft)", "Immanuel Kant", "856", "1781"}, 
-        {"Beyond Good and Evil (Jenseits von Gut und Böse)", "Friedrich Nietzsche", "268", "1886"}, 
-        {"Simulacra and Simulation (Simulacres et Simulation)", "Jean Baudrillard", "164", "1981"}, 
-        {"Filosofi Teras", "Henry Manampiring", "346", "2018"}
+        {"Being and Time (Sein und Zeit)", "Martin Heidegger", "589", "1927","Menyelidiki eksistensi manusia dan konsep waktu secara mendalam."}, 
+        {"Critique of Pure Reason (Kritik der reinen Vernunft)", "Immanuel Kant", "856", "1781","Analisis kritis tentang kemampuan akal manusia dalam pemahaman."}, 
+        {"Beyond Good and Evil (Jenseits von Gut und Böse)", "Friedrich Nietzsche", "268", "1886","Memperdebatkan moralitas dan etika dalam pandangan yang lebih luas."}, 
+        {"Simulacra and Simulation (Simulacres et Simulation)", "Jean Baudrillard", "164", "1981","Diskusi tentang realitas, simulasi, dan peran tanda."}, 
+        {"Filosofi Teras", "Henry Manampiring", "346", "2018","Eksplorasi filsafat fundamental tentang hakikat kehidupan dan eksistensi."}
     };
 
     String[][] sejarah = {
-        {"A People's History of the United States", "Howard Zinn", "729", "1980"}, 
-        {"The Guns of August", "Barbara W. Tuchman", "511", "1962"}, 
-        {"The Silk Roads: A New History of the World", "Peter Frankopan", "636", "2015"}, 
-        {"SPQR: A History of Ancient Rome", "Mary Beard", "608", "2015"}, 
-        {"A History of the World in 6 Glasses", "Tom Standage", "336", "2005"}
+        {"A People's History of the United States", "Howard Zinn", "729", "1980","Sejarah alternatif AS dari sudut pandang rakyat."}, 
+        {"The Guns of August", "Barbara W. Tuchman", "511", "1962","Analisis tentang pemicu dan konsekuensi Perang Dunia I."}, 
+        {"The Silk Roads: A New History of the World", "Peter Frankopan", "636", "2015","Menyelidiki peran jalur sutra dalam sejarah dunia."}, 
+        {"SPQR: A History of Ancient Rome", "Mary Beard", "608", "2015","Narasi mendalam tentang kekaisaran Romawi kuno."}, 
+        {"A History of the World in 6 Glasses", "Tom Standage", "336", "2005","Menjelajahi sejarah dunia melalui lensa minuman."}
     };
 
     String[][] agama = {
-        {"The Case for Christ", "Lee Strobel", "464", "1998"}, 
-        {"The God Delusion", "Richard Dawkins", "464", "2006"}, 
-        {"Mere Christianity", "C.S. Lewis", "175", "1952"}, 
-        {"The Tao of Pooh", "Benjamin Hoff", "158", "1982"}, 
-        {"The Art of Happiness", "Dalai Lama XIV dan Howard Cutler", "336", "1998"}
+        {"The Case for Christ", "Lee Strobel", "464", "1998","Investigasi jurnalistik tentang keberadaan Yesus Kristus."}, 
+        {"The God Delusion", "Richard Dawkins", "464", "2006","Debat kontroversial tentang eksistensi Tuhan dan agama."}, 
+        {"Mere Christianity", "C.S. Lewis", "175", "1952","Penjelasan filosofis tentang dasar-dasar kekristenan."}, 
+        {"The Tao of Pooh", "Benjamin Hoff", "158", "1982","Pengantar ringan tentang prinsip Taoisme melalui karakter Winnie the Pooh."}, 
+        {"The Art of Happiness", "Dalai Lama XIV dan Howard Cutler", "336", "1998","Pemahaman Buddhisme Tibet tentang mencapai kebahagiaan."}
     };
 
     String[][] psikologi = {
-        {"Thinking, Fast and Slow", "Daniel Kahneman", "499", "2011"}, 
-        {"The Power of Habit: Why We Do What We Do in Life and Business", "Charles Duhigg", "392", "2012"}, 
-        {"Man's Search for Meaning", "Viktor E. Frankl", "200", "1946"}, 
-        {"Influence: The Psychology of Persuasion", "Robert B. Cialdini", "592", "1984"}, 
-        {"Quiet: The Power of Introverts in a World That Can't Stop Talking", "Susan Cain", "333", "2012"}
+        {"Thinking, Fast and Slow", "Daniel Kahneman", "499", "2011","Menjelaskan tentang dua mode berpikir manusia: cepat dan lambat."}, 
+        {"The Power of Habit: Why We Do What We Do in Life and Business", "Charles Duhigg", "392", "2012","Memahami kebiasaan manusia dan bagaimana mempengaruhinya."}, 
+        {"Man's Search for Meaning", "Viktor E. Frankl", "200", "1946","Kisah pengalaman selama perang dan pencarian arti hidup."}, 
+        {"Influence: The Psychology of Persuasion", "Robert B. Cialdini", "592", "1984","Menjelaskan mekanisme persuasi dan pengaruhnya dalam kehidupan."}, 
+        {"Quiet: The Power of Introverts in a World That Can't Stop Talking", "Susan Cain", "333", "2012","Mengulas kekuatan introvert dalam dunia yang cenderung ekstrovert."}
     };
 
-    String[][] 
-    politik = {
-        {"The Prince", "Niccolò Machiavelli", "164", "1532"}, 
-        {"The Road to Serfdom", "Friedrich Hayek", "266", "1944"}, 
-        {"The Audacity of Hope: Thoughts on Reclaiming the American Dream", "Barack Obama", "362", "2006"}, 
-        {"Politik Kuasa Media", "Noam Chomsky", "166", "2022"}, 
-        {"The Conscience of a Conservative", "Barry Goldwater", "123", "1960"}
+    String[][] politik = {
+        {"The Prince", "Niccolò Machiavelli", "164", "1532","Buku klasik tentang politik dan kekuasaan."}, 
+        {"The Road to Serfdom", "Friedrich Hayek", "266", "1944","Kritik terhadap ideologi sosialis dan ancaman tirani."}, 
+        {"The Audacity of Hope: Thoughts on Reclaiming the American Dream", "Barack Obama", "362", "2006","Pemikiran mantan Presiden AS tentang harapan dan impian."}, 
+        {"Politik Kuasa Media", "Noam Chomsky", "166", "2022","Analisis tentang pengaruh media dalam politik."}, 
+        {"The Conscience of a Conservative", "Barry Goldwater", "123", "1960","Manifesto konservatif Amerika tentang kebebasan dan pemerintahan."}
     };
 
-    String[][] 
-    fiksi = {
-        {"Neuromancer", "William Gibson", "271", "1984"}, 
-        {"Snow Crash", "Neal Stephenson", "480", "1992"}, 
-        {"Ready Player One", "Ernest Cline", "546", "2011"}, 
-        {"The Circle", "Dave Eggers", "504", "2013"}, 
-        {"Do Androids Dream of Electric Sheep?", "Philip K. Dick", "210", "1968"}
+    String[][] fiksi = {
+        {"Neuromancer", "William Gibson", "271", "1984","Kisah cyberpunk tentang seorang hacker di dunia maya."}, 
+        {"Snow Crash", "Neal Stephenson", "480", "1992","Cerita distopia tentang realitas virtual dan kekuatan bahasa."}, 
+        {"Ready Player One", "Ernest Cline", "546", "2011"," Petualangan di dunia virtual yang penuh dengan referensi budaya pop."}, 
+        {"The Circle", "Dave Eggers", "504", "2013","Penggambaran gelombang teknologi dan privasi dalam media sosial."}, 
+        {"Do Androids Dream of Electric Sheep?", "Philip K. Dick", "210", "1968","Cerita fiksi ilmiah tentang hubungan antara manusia dan android."}
     };
 }
